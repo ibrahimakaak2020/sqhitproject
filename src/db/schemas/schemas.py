f39d901385bb83587c2d150ecae3c8c5e29995a7
ibrahimakaak2020 <ibrahimakaak@gmail.com>
2022-08-30 12:14:03 +0400

    initial commit

---
+++ src/db/schemas/schemas.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel,EmailStr
+
+class ManufactureCreate( BaseModel ):
+    company_name : str
+
+    class Config:  # to convert non dict obj to json
+        orm_mode = True
+ 
+
+
+class EquipmentTypeCreate(BaseModel):
+
+           equipment_type : str
+
+           class Config:  # to convert non dict obj to json
+             orm_mode = True
+    
+
+
+
+class LocationCreate(BaseModel):
+
+    loc_name : str
+    building : str
+    contact_number : str
+
+    class Config:  # to convert non dict obj to json
+        orm_mode = True
+
+class EquipmentCreate(BaseModel):
+    sn:str
+    emid:int
+    locid:int
+    class Config:
+        orm_mode=True
+   
+class UserCreate(BaseModel):
+    staffno:int
+    staffname:str
+    password:str
+
+    class Config:
+        orm_mode=True
+
+
+class EquipmentCreate(BaseModel):
+    sn:str
+    emid:int
+    locid:int
+    class Config:
+        orm_mode=True
+
+
+
+
+class EquipmentRegisterCreate(BaseModel):
+    sn:str
+    register_by:int
+    register_desc:str
+    workorderid:str
+   
+
+    class Config:
+        orm_mode=True
+
+
+
+class EquipmentActivityCreate(BaseModel):
+    registerid:int
+    create_by:int
+    company_id:int=None
+    activity_desc:str
+    next_activity:str=None
+    activity_status:str
+    place_of_maintaince:str
+    maintaince_status:str=None
+    billid:Optional[str]=None
+    billamount:Optional[int]=0
+    date_of_send:Optional[datetime]=None
+    date_of_returnback:Optional[datetime]=None
+    date_of_recievefrom:Optional[datetime]=None
+    date_of_maintaince:Optional[datetime]=None
+
+   
+
+    class Config:
+        orm_mode=True
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from db.database.database import get_db as db
+
+print(db())
+
+print(db())
+
+print("welcome")
