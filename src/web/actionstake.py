2f3bb9c3ac8d6f2ec176ac24a25492aa6e89c37a
ibrahimakaak2020 <59326489+ibrahimakaak2020@users.noreply.github.com>
2022-12-05 13:23:02 +0400

    05 22 22 05

--- src/web/actionstake.py
+++ src/web/actionstake.py
@@ -5,19 +5,19 @@ def actions(activity:EquipmentActivity=None):
     activity_status=activity.activity_status
 
     if activity_status=='UPL':
-        return {'Repaire Status':'locally', 'candemnational': '/candem'}
+        return {'Repaire Status':'locally', 'candemnational': 'candem'}
 
     if activity_status=='WFS':
-        return {'Send': '/send', 'keep for decision': '/keepfor', 'candemnational': '/candem'}
+        return {'Send': 'send', 'keep for decision': 'keepfor', 'candemnational': 'candem'}
     if activity_status=='UPS':
         return {'Recieve': 'recievefromcompany'}
     if activity_status=='WFD':
-        return {'Send To Company': '/send', 'Repaire On IT Workshop': '/locally', 'candemnational': '/candem'}
+        return {'Send To Company': 'send', 'Repaire On IT Workshop': 'locally', 'candemnational': 'candem'}
     if activity_status=='WFR':
-        return {'Return Back': '/return','keep for decision': '/keepfor'}
+        return {'Return Back To Deparmtment': 'returnbackto','keep for decision': 'keepfor'}
    
     else:
-        return {'Send To Company': '/send','Waiting for Send':'/waitingfor', 'Repaire On IT Workshop': '/locally', 'candemnational': '/candem'}
+        return {'Send To Company': 'send','Waiting for Send':'waitingfor', 'Repaire On IT Workshop': 'locally', 'candemnational': 'candem'}
 
 
 
