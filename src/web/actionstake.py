bddfc06874183a1362503a8d1d5abd3d22e5560e
ibrahimakaak2020 <59326489+ibrahimakaak2020@users.noreply.github.com>
2022-12-05 11:48:53 +0400

    home 5 12

--- src/web/actionstake.py
+++ src/web/actionstake.py
@@ -5,12 +5,12 @@ def actions(activity:EquipmentActivity=None):
     activity_status=activity.activity_status
 
     if activity_status=='UPL':
-        return {'Repaired': '/repaired', 'Notrepaired':'/notrepaired', 'candemnational': '/candem'}
+        return {'Repaire Status':'locally', 'candemnational': '/candem'}
 
     if activity_status=='WFS':
         return {'Send': '/send', 'keep for decision': '/keepfor', 'candemnational': '/candem'}
     if activity_status=='UPS':
-        return {'Recieve Repaired': '/repaired', 'Recieve Not Repaired': '/notrepaired'}
+        return {'Recieve': 'recievefromcompany'}
     if activity_status=='WFD':
         return {'Send To Company': '/send', 'Repaire On IT Workshop': '/locally', 'candemnational': '/candem'}
     if activity_status=='WFR':
