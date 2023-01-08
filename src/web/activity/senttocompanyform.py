088a81540b19d6b2f61cff238cc2f54a4b08a348
ibrahimakaak2020 <59326489+ibrahimakaak2020@users.noreply.github.com>
2023-01-08 13:59:39 +0400

    sf asfsa f

--- src/web/activity/senttocompanyform.py
+++ src/web/activity/senttocompanyform.py
@@ -36,11 +36,8 @@ class SendToCompanyForm:
     async def is_valid(self,registerid=None,db=None):
         
         company_id = QueryModelData(modeltable=Company_User, db=db, cols={"cid": self.company_id}).all()
-        equipment = QueryModelData(modeltable=Equipment, db=db, cols={"sn": registerid.sn}).all()
-        print(equipment,'--------------------========================')
-        print(registerid.registerid,'--------------------========================')
+        equipment = QueryModelData(modeltable=Equipment, db=db, cols={"sn": self.sn}).all()
         
-      
         if equipment:
             register = QueryModelData(modeltable=EquipmentRegister,db=db , cols={"registerid": registerid.registerid,"register_status":"Y"} ).first()
             if not register:
