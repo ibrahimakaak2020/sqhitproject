44a966e34f4d61a78d4ea1ba0c8479cb1e688ad0
ibrahimakaak2020 <59326489+ibrahimakaak2020@users.noreply.github.com>
2023-01-08 10:51:59 +0400

    8 1 23

--- src/static/my.js
+++ src/static/my.js
@@ -22,3 +22,25 @@ function myFunction() {
 
 //If the button to open the modal is clicked, then
 //it should open the modal. ids are used to refer to the HTML elements
+
+function searchTable() {
+    var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
--- src/web/actionstake.py
+++ src/web/actionstake.py
@@ -5,19 +5,19 @@ def actions(activity:EquipmentActivity=None):
     activity_status=activity.activity_status
 
     if activity_status=='UPL':
-        return {'Repaire Status':'locally', 'candemnational': 'candem'}
+        return {'Repaire Status':'locally'}
 
     if activity_status=='WFS':
-        return {'Send': 'send', 'keep for decision': 'keepfor', 'candemnational': 'candem'}
+        return {'Send': 'send', 'keep for decision': 'keepfor'}
     if activity_status=='UPS':
         return {'Recieve': 'recievefromcompany'}
     if activity_status=='WFD':
-        return {'Send To Company': 'send', 'Repaire On IT Workshop': 'Workshop', 'Return Back To Deparmtment': 'returnbackto','candemnational': 'candem'}
+        return {'Send To Company': 'send', 'Repaire On IT Workshop': 'Workshop', 'Return Back To Deparmtment': 'returnbackto'}
     if activity_status=='WFR':
         return {'Return Back To Deparmtment': 'returnbackto','keep for decision': 'keepfor'}
    
     else:
-        return {'Send To Company': 'send','Waiting for Send':'waitingfor', 'Repaire On IT Workshop': 'Workshop', 'candemnational': 'candem'}
+        return {'Send To Company': 'send','Waiting for Send':'waitingfor', 'Repaire On IT Workshop': 'Workshop'}
 
 
 
